6c5170de2d726696fc04801e5e0c8d190ccea7a7
paulinewang <wbl.pauline@gmail.com>
2020-05-08 20:50:59 +0200

    Remove unnecessary assets and manipulate country data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Search from "./components/Search";
+// import CountryGroup from "./components/CountryGroup";
 
 function App() {
   const [allCountries, setAllCountries] = useState([]);
@@ -11,21 +12,41 @@ function App() {
       const response = await fetch("https://restcountries.eu/rest/v2/all");
       let data = await response.json();
 
-      // Filter the properties so that we only keep name and capital for now
+      // Filter the properties — only keep name and capital for now
       let countries = [];
-      data.map(({ name, capital }) => {
-        countries.push({ name, capital });
-      });
+      data.map(({ name, capital }) => countries.push({ name, capital }));
       setAllCountries(countries);
     };
 
     fetchCountries();
   }, []);
 
+  useEffect(() => {
+    var alphabeticallyGroupedCountries = [];
+    var i, j;
+    // Converting char codes to the letters
+    for (i = 0; i < 26; i++) {
+      // Creating the object properties that match the letter
+      alphabeticallyGroupedCountries.push({
+        [String.fromCharCode(i + 65)]: [],
+      });
+      // Run through every country and check the first letter
+      for (j = 0; j < allCountries.length; j++) {
+        if (allCountries[j]["name"].charAt(0) === String.fromCharCode(i + 65)) {
+          const array =
+            alphabeticallyGroupedCountries[i][String.fromCharCode(i + 65)];
+          array.push(allCountries[j]);
+        }
+      }
+    }
+    console.log(alphabeticallyGroupedCountries);
+  });
+
   return (
     <div className="App">
       <h1>Travel app</h1>
       <Search />
+      {/* <CountryGroup countries={allCountries} /> */}
     </div>
   );
 }
