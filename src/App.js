9aad1317be6432ad04ae4849ccf1246a0667e285
paulinewang <wbl.pauline@gmail.com>
2020-05-08 19:41:13 +0200

    Create base files and fetch country data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import Search from "./components/Search";
+
 function App() {
-  return <div className="App">Travel app</div>;
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch("https://restcountries.eu/rest/v2/all");
+      let data = await response.json();
+
+      // Filter the properties so that we only keep name and capital for now
+      let countries = [];
+      data.map(({ name, capital }) => {
+        countries.push({ name, capital });
+      });
+      setAllCountries(countries);
+    };
+
+    fetchCountries();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Travel app</h1>
+      <Search />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./../styles/search.scss";
+
+const Search = () => {
+  return <input type="text" placeholder="Search countries.." />;
+};
+
+export default Search;
